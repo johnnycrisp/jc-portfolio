b9c890fad5595dd6c22ac7e4d888ac5e46cb7f0e
Johnny Crisp <johnnycrisp2@gmail.com>
2022-12-09 15:03:39 +0100

    about page first design

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,10 +6,11 @@ import { StaticImage } from 'gatsby-plugin-image'
 
 const about = () => {
   return (
-    <div className="about__page-wrapper">
+    
     <Layout>
     {/* <Seo title="About" /> */}
-   
+   <div id="cursor"></div>
+    <div id="cursor-border"></div>
     {/* <h1>
         <span className="letter">A</span>
         <span className="letter">b</span>
@@ -19,34 +20,26 @@ const about = () => {
         <span className="letter">.</span>
     </h1> */}
     <div className="about__wrapper">
-        <section className="about__image">
-            <div>
+        <section>
+            <div className="about__image">
                 <StaticImage className="about__image" src="../assets/images/snoopy.jpg" alt="running man" />
             </div>
-        </section>
-        <section className="about__text">
-            <div>
+            <div className="about__text">
                 <h2>Experience</h2>
                 <p>I've had a few job titles over the years, including journalist, translator, UX copywriter, and technical writer. For the past 6+ years, I've been working hand-in-hand with executive, marketing, and dev teams to create seamless digital experiences.</p>
             </div>
         </section>
-        <section className="about__image">
-            <div>
+        <section>
+             <div className="about__image">
                 <StaticImage className="about__image" src="../assets/images/snoopy.jpg" alt="running man" />
             </div>
-        </section>
-        <section className="about__text">
-            <div>
-                <h2>Education</h2>
-                <p>I am a Treehouse Techdegree Frontend Web Development graduate who is also currently learning a MERN stack via the University of Helsinki's Full Stack Open programme.</p>
+            <div className="about__text">
+                <h2>Skills</h2>
+                <p>I've had a few job titles over the years, including journalist, translator, UX copywriter, and technical writer. For the past 6+ years, I've been working hand-in-hand with executive, marketing, and dev teams to create seamless digital experiences.</p>
             </div>
-        </section> 
-         <section>
-            <h2>Skills</h2>
-         </section>
+        </section>
     </div>
   </Layout>
-  </div>
   )
 }
 
