ee74c1794f76c2c14d293b9997f8de64cf6190c2
Johnny Crisp <johnnycrisp2@gmail.com>
2022-12-09 16:49:47 +0100

    new about design

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -20,16 +20,12 @@ const about = () => {
         <span className="letter">.</span>
     </h1> */}
     <div className="about__wrapper">
-        <section>
-            <div className="about__image">
-                <StaticImage className="about__image" src="../assets/images/snoopy.jpg" alt="running man" />
-            </div>
+        <section className="about__intro">
             <div className="about__text">
-                <h2>Experience</h2>
-                <p>I've had a few job titles over the years, including journalist, translator, UX copywriter, and technical writer. For the past 6+ years, I've been working hand-in-hand with executive, marketing, and dev teams to create seamless digital experiences.</p>
+                <p>Hi, I'm Johnny 👋. After 6+ years as a SaaS copywriter, I'm now focussed on creating very nice Jamstack websites (with React, Gatsby, DatoCMS, and Netlify.) </p>
             </div>
         </section>
-        <section>
+        {/* <section>
              <div className="about__image">
                 <StaticImage className="about__image" src="../assets/images/snoopy.jpg" alt="running man" />
             </div>
@@ -37,7 +33,7 @@ const about = () => {
                 <h2>Skills</h2>
                 <p>I've had a few job titles over the years, including journalist, translator, UX copywriter, and technical writer. For the past 6+ years, I've been working hand-in-hand with executive, marketing, and dev teams to create seamless digital experiences.</p>
             </div>
-        </section>
+        </section> */}
     </div>
   </Layout>
   )
